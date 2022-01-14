ddb1454d9de4600efcecf797fcfdd08649b5bcdc
Rogerio bispo <rogerio.bispo@nexaas.com>
2022-01-13 22:23:09 -0300

    validation to max field size

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@
 class User < ApplicationRecord
   validates :email, :phone_number, :key, presence: true
   validates :email, :phone_number, :key, :account_key, uniqueness: true
+  validates :metadata, length: { maximum: 2000 }
+  validates :phone_number, length: { maximum: 20 }
+  validates :email, :full_name, length: { maximum: 200 }
+  validates :password, :key, :account_key, length: { maximum: 100 }
 
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,14 +7,27 @@ RSpec.describe User, type: :model do
     described_class.new(email: 't@t.com', phone_number: '1234567890', password: '12345678', key: '1234567890')
   end
 
-  context 'with validations' do
+  context 'with validations presence_of' do
     it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to validate_presence_of(:phone_number) }
-    it { is_expected.to validate_uniqueness_of(:phone_number).case_insensitive }
     it { is_expected.to validate_presence_of(:key) }
+  end
+
+  context 'with validation uniqueness_of' do
+    it { is_expected.to validate_uniqueness_of(:phone_number).case_insensitive }
     it { is_expected.to validate_uniqueness_of(:key).case_insensitive }
     it { is_expected.to validate_uniqueness_of(:account_key) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+  end
+
+  context 'with validation max filed size' do
+    it { is_expected.to validate_length_of(:email).is_at_most(200) }
+    it { is_expected.to validate_length_of(:phone_number).is_at_most(20) }
+    it { is_expected.to validate_length_of(:full_name).is_at_most(200) }
+    it { is_expected.to validate_length_of(:password).is_at_most(100) }
+    it { is_expected.to validate_length_of(:key).is_at_most(100) }
+    it { is_expected.to validate_length_of(:account_key).is_at_most(100) }
+    it { is_expected.to validate_length_of(:metadata).is_at_most(2000) }
   end
 
   context 'with encrypted password' do
